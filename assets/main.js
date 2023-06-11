6e8e088941f3f995ddbecf539915cf6ad7efb707
RA MEY <129702894+RAMEY0406@users.noreply.github.com>
2023-06-11 12:26:43 +0700

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,19 @@
+const toggleBtn = document.querySelector('.toggle_btn');
+    const toggleBtnIcon = document.querySelector('.toggle_btn i');
+    const dropDownMenu = document.querySelector('.dropdown_menu');
+    toggleBtn.onclick = function (){
+        dropDownMenu.classList.toggle('open')
+        const isOpen = dropDownMenu.classList.contains('open')
+
+        toggleBtnIcon.classList = isOpen
+        ? 'fa-solid fa-xmark'
+        : 'fa-solid fa-bars'
+}
+
+
+const body = document.body;
+    function changeMode(){
+        setTimeout (()=>{
+            body.classList.toggle("blackMode");
+        });
+    }
